7a34aa7beecae4db24c7974e15dfc89b5a4a498e
Alexey Jukov <nitroalexus@gmail.com>
2024-09-06 01:01:14 +0300

    rotation of energy

--- Assets/Scripts/Controllers/Energy.cs
+++ Assets/Scripts/Controllers/Energy.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Energy : MonoBehaviour
 {
     [SerializeField] int energyValue = 100;
-
+    [SerializeField] float rotationDuration = 3f;
     public int EnergyValue { get { return energyValue; } set { energyValue = value; } }
 
+    private void Start()
+    {
+        transform.DORotate(new Vector3(0, 180, 0), rotationDuration, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Incremental)
+            .SetEase(Ease.Linear);
+    }
 }
