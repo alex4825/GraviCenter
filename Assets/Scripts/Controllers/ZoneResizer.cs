82d35282f50f30d485749d7a61061de2a9ab0acc
Alexey Jukov <nitroalexus@gmail.com>
2024-09-07 03:36:29 +0300

    Finish portal created

---
+++ Assets/Scripts/Controllers/ZoneResizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneResizer : MonoBehaviour
+{
+    [SerializeField] float zoneRadius = 10;
+    void Start()
+    {
+        Transform zoneTransform = Searcher.FindChildWithTag(transform, "Zone");
+
+        const float DEFAULT_SCALE = 10; //10 units
+
+        float scale = zoneRadius / DEFAULT_SCALE;
+
+        GraviCenter GC = new GraviCenter(); //if this file attached to GC
+        if (TryGetComponent<GraviCenter>(out GC))
+        {
+            scale = GC.GravityZone / DEFAULT_SCALE;
+        }
+
+        zoneTransform.localScale = new Vector3(scale, scale, scale);
+    }
+}
